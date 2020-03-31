f1b71349b9c08c059d379dbff760c5bb6a80114b
Radvil Ardian <radvil.developer@gmail.com>
2020-03-31 20:29:53 +0800

    TODO: Fix register component on small device

--- projects/ngrx-app/src/app/features/auth/register/register.component.ts
+++ projects/ngrx-app/src/app/features/auth/register/register.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/utils/animations';
 
 @Component({
 	selector: 'rad-register',
 	templateUrl: './register.component.html',
-	styleUrls: ['./register.component.scss']
+	styleUrls: ['./register.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterComponent implements OnInit {
+	routerElementsAnimations = ROUTE_ANIMATIONS_ELEMENTS;
+
 	constructor() {}
 
 	ngOnInit(): void {}
